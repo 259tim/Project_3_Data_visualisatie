a3320ecda46d948b3dd142907de154d6636ff4c5
Jaap <dijkvanjaap@hotmail.com>
2018-02-21 21:16:10 +0100

    Add project files.

---
+++ Project3Datavisualisatie/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿using MaterialSkin;
+using Project3Datavisualisatie.Properties;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Project3Datavisualisatie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.currentYear = new System.Windows.Forms.TextBox();
+            this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Controls.Add(this.tabPage2);
+            this.materialTabControl1.Controls.Add(this.tabPage3);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Location = new System.Drawing.Point(-2, 49);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(610, 743);
+            this.materialTabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.currentYear);
+            this.tabPage1.Controls.Add(this.trackBar);
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(602, 717);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 108);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Criminaliteit";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(563, 210);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(602, 717);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(602, 717);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(75, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Location = new System.Drawing.Point(2, 0);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(606, 48);
+            this.materialTabSelector1.TabIndex = 1;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Henk";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(39, 324);
+            this.trackBar.Maximum = 2016;
+            this.trackBar.Minimum = 2005;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(239, 45);
+            this.trackBar.TabIndex = 2;
+            this.trackBar.Value = 2005;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // currentYear
+            // 
+            this.currentYear.Location = new System.Drawing.Point(39, 376);
+            this.currentYear.Name = "currentYear";
+            this.currentYear.Size = new System.Drawing.Size(100, 20);
+            this.currentYear.TabIndex = 3;
+            this.currentYear.Text = "2005";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 769);
+            this.Controls.Add(this.materialTabSelector1);
+            this.Controls.Add(this.materialTabControl1);
+            this.Name = "Form1";
+            this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private void customForm()
+        {
+            //custom form properties
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            Location = new Point(workingArea.Right - 435, workingArea.Top);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            Width = 435;
+            Height = Screen.PrimaryScreen.WorkingArea.Height;
+            //materialskin
+            var skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.DARK; //LightBlue200
+            skinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.Orange800, Primary.BlueGrey500, Accent.Orange700, TextShade.WHITE);
+        }
+
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
+        private NotifyIcon notifyIcon;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private TrackBar trackBar;
+        private TextBox currentYear;
+    }
+}
+
---
+++ Project3Datavisualisatie/Form1.cs
@@ -0,0 +1,140 @@
+﻿using MaterialSkin.Controls;
+using Microsoft.VisualBasic.FileIO;
+using Project3Datavisualisatie.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project3Datavisualisatie
+{
+    public partial class Form1 : MaterialForm
+    {
+        private bool Slided;
+        public Form1()
+        {
+            InitializeComponent();
+            customForm();
+
+            Slided = false;
+            Toggle();
+
+            //this.chart1.Series["Criminaliteit"].Points.AddXY("rotterdam", 123);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        
+     
+        
+        //Form slide toggle
+        public void Toggle()
+        {
+            if (Slided)
+            {
+                SlideOut(this, 435);
+                Slided = false;
+            }
+            else
+            {
+                SlideIn(this, 0);
+                Slided = true;
+            }
+        }
+
+        //Form slideout
+        private async void SlideOut(Form1 o, int move)
+        {
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            while (move > 0)
+            {
+                await Task.Delay(10);
+                int a = move -= 29;
+                Location = new Point(workingArea.Right - move, workingArea.Top);
+            }
+            move = 0;
+        }
+        //Form slidein
+        private async void SlideIn(Form1 o, int move)
+        {
+            Slided = true;
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            while (move < 435)
+            {
+                await Task.Delay(10);
+                int a = move += 29;
+                Location = new Point(workingArea.Right - move, workingArea.Top);
+            }
+            move = 435;
+        }
+
+        //Hotkey for sliding
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Escape))
+            {
+                Toggle();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        
+        //Toggle slide on clicking the icon
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            Toggle();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            string year = trackBar.Value.ToString();
+            currentYear.Text = year;
+            if (year == "2005") {
+            this.chart1.Series["Criminaliteit"].Points.Clear();
+            this.chart1.Series["Criminaliteit"].Points.AddXY("rotterdam", 123);
+            this.chart1.Series["Criminaliteit"].Points.AddXY("jemoeder", 69);
+            }
+            if (year == "2006")
+            {
+                this.chart1.Series["Criminaliteit"].Points.Clear();
+                this.chart1.Series["Criminaliteit"].Points.AddXY("rotterdam", 23);
+                this.chart1.Series["Criminaliteit"].Points.AddXY("jemoeder", 12);
+            }
+            if (year == "2007")
+            {
+                this.chart1.Series["Criminaliteit"].Points.Clear();
+                this.chart1.Series["Criminaliteit"].Points.AddXY("rotterdam", 210);
+                this.chart1.Series["Criminaliteit"].Points.AddXY("jemoeder", 69);
+            }
+            if (year == "2008")
+            {
+                this.chart1.Series["Criminaliteit"].Points.Clear();
+                this.chart1.Series["Criminaliteit"].Points.AddXY("rotterdam", 50);
+                this.chart1.Series["Criminaliteit"].Points.AddXY("jemoeder", 20);
+            }
+            if (year == "2009")
+            {
+                this.chart1.Series["Criminaliteit"].Points.Clear();
+                this.chart1.Series["Criminaliteit"].Points.AddXY("rotterdam", 123);
+                this.chart1.Series["Criminaliteit"].Points.AddXY("jemoeder", 69);
+            }
+            if (year == "20010")
+            {
+                this.chart1.Series["Criminaliteit"].Points.Clear();
+                this.chart1.Series["Criminaliteit"].Points.AddXY("rotterdam", 20);
+                this.chart1.Series["Criminaliteit"].Points.AddXY("jemoeder", 40);
+            }
+        }
+    }
+}
